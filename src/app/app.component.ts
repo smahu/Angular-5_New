fd2ca8ac937a801bd9487983352190d0a0280384
Manoj Shukla <shuklamanoj119@gmail.com>
2018-01-17 16:29:39 +0530

    Simple App Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,12 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
+
+import { Component} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent{
-  afterLogin = false;
-  constructor(public router: Router) {
-
-  }
-
-  OnClick(){
-    this.afterLogin=true;
-    this.router.navigate(['/user']);
-
+export class AppComponent {
+  constructor() {
   }
 }
-
